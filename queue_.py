a939e5be611464f3804ba429416b8e6fe89fa8e6
McRavioli <153536618+McRavioli@users.noreply.github.com>
2024-07-24 20:43:41 +0200

    combined the handleing for youtube and soundcloud and removed pytube
    Pytube had some problems on their side which completely broke the YouTube functionality. That’s why YouTube links are now also handled via youtube-dl

--- queue_.py
+++ queue_.py
@@ -1,17 +1,20 @@
 from discord import FFmpegPCMAudio
 import asyncio
-from pytube import YouTube
 from yt_dlp import YoutubeDL
 from config import FFMPEG_OPTIONS, ydl_opts
 from bot_instance import bot
 from RandoFileToPreventCircularImport import queue, queue_count, current_song, song_count
 
-
 ydl_opts = {
     'format': 'bestaudio/best',
     'quiet': True,
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
 }
-
 
 #queue error handling
 def check_queue(ctx, error):
@@ -21,7 +24,6 @@ def check_queue(ctx, error):
         fut.result()
     except:
         pass
-
 
 async def play_next(ctx):
     global queue, voice_channel, queue_count, url, video_title, song_count, current_song_url, current_song_title, ydl_opts
@@ -35,21 +37,19 @@ async def play_next(ctx):
         song_count += 1
         if song_count > 1:
             await ctx.reply('NEXT')
-        if 'soundcloud' in url:
-            # handle SoundCloud link
-            with YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=False)
-                video_title = info_dict['title']
-                stream_url = info_dict['url']
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            video_title = info_dict.get('title', 'Unknown Title')
+            stream_url = info_dict.get('url')
+
+            if stream_url:
                 source = FFmpegPCMAudio(stream_url, **FFMPEG_OPTIONS)
-        else:
-            # handle YouTube link
-            youtube = YouTube(url)
-            video_title = youtube.title
-            stream = youtube.streams.filter(only_audio=True).first()
-            source = FFmpegPCMAudio(stream.url, **FFMPEG_OPTIONS)
-        voice_channel.play(source, after=lambda e: check_queue(ctx, e))
-        current_song[ctx.guild.id] = {'title': video_title, 'url': url}
-        print(f"Updated current song for guild {ctx.guild.id}: {current_song[ctx.guild.id]}")  # Debug print
+                voice_channel.play(source, after=lambda e: check_queue(ctx, e))
+                current_song[ctx.guild.id] = {'title': video_title, 'url': url}
+                print(f"Updated current song for guild {ctx.guild.id}: {current_song[ctx.guild.id]}")  # Debug print
+            else:
+                print(f"Failed to extract stream URL for {url}")
+
     if queue.empty():
         song_count = 0
